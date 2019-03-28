278f06260daee1fe55f48df3389ca8971e1a033b
Gregory Losco <gtlosco@gmail.com>
2019-03-28 00:14:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,8 @@ def line(array)
   if array.size == 0
     puts "The line is currently empty."
   else 
-  binding.pry
+    array.each_with_index.collect {|name, index|
+    "#{index+1}. #{name}"
+    }
 end
 end
