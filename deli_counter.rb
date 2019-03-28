09dcf7acbef6deafec0854ac0e756d5cf3cbb13c
Gregory Losco <gtlosco@gmail.com>
2019-03-28 00:19:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,10 @@ def line(array)
   if array.size == 0
     puts "The line is currently empty."
   else 
-    array.each_with_index.collect {|name, index|
-    puts "The line is currently: #{index+1}. #{name}"
+    new_array = []
+    array.each_with_index {|name, index|
+    new_array << "#{index+1}. #{name}"
     }
 end
+    puts "The line is currently: new_array"
 end
