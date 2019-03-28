8c27c54426a9579f346d9b2e5eabc2def8e45ed7
Gregory Losco <gtlosco@gmail.com>
2019-03-28 00:05:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def line(customer)
+  if customer.size >= 1 
+    index = 1
+    new_array = []
+    customer.each_with_index {|customer, index| new_array << "#{index+1}. #{customer}"
+      index += 1
+    }
+    puts "The line is currently: #{new_array.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(customer, name)
+  
+  puts "Welcome, #{name}. You are number #{index} in line."
+end
